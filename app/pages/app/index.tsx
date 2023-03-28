0de8c3f9f99c72c59de15e8ac73b209069941f2b
CrispyyBaconx <immabaconback@gmail.com>
2023-03-28 10:12:02 -0400

    Minor Fixes

--- app/pages/app/index.tsx
+++ app/pages/app/index.tsx
@@ -1,12 +1,14 @@
 import { Head } from "next/document";
 import styles from "@/styles/App.module.css"
 
-
 const app = () => {
     return (
         <>
             <Head>
-                {/* Add meta tags later */}
+                <title>Stakex</title>
+        		<meta name="description" content="Stakex" />
+        		<meta name="viewport" content="width=device-width, initial-scale=1" />
+        		<link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
                 <header>
