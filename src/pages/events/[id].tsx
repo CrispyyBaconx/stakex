7cadae2d403df040739ecf9a9b99c3fb78f59f32
CrispyyBaconx <immabaconback@gmail.com>
2023-04-06 09:46:56 -0400

    Edited Spacing

--- src/pages/events/[id].tsx
+++ src/pages/events/[id].tsx
@@ -4,52 +4,52 @@ import { User } from '../../interfaces'
 import { sampleUserData } from '../../utils/sample-data'
 
 type Props = {
-  item?: User
-  errors?: string
+  	item?: User
+  	errors?: string
 }
 
 const StaticPropsDetail = ({ item, errors }: Props) => {
-  if (errors) {
-    return (
-      <>
-        <p>
-          <span style={{ color: 'red' }}>Error:</span> {errors}
-        </p>
-      </>
-    )
-  }
+  	if (errors) {
+    	return (
+      		<>
+        		<p>
+          			<span style={{ color: 'red' }}>Error:</span> {errors}
+        		</p>
+      		</>
+    	)
+  	}
 
-  return (
-    <>
-      <span>Hi</span>
-    </>
-  )
+  	return (
+  	  	<>
+  	    	<span>Hi</span>
+  	  	</>
+  	)
 }
 
 export default StaticPropsDetail
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  // Get the paths we want to pre-render based on users
-  const paths = sampleUserData.map((user) => ({
-    params: { id: user.id.toString() },
-  }))
+  	// Get the paths we want to pre-render based on users
+  	const paths = sampleUserData.map((user) => ({
+  	  	params: { id: user.id.toString() },
+  	}))
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false }
+  	// We'll pre-render only these paths at build time.
+  	// { fallback: false } means other routes should 404.
+  	return { paths, fallback: false }
 }
 
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
 // direct database queries.
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  try {
-    const id = params?.id
-    const item = sampleUserData.find((data) => data.id === Number(id))
-    // By returning { props: item }, the StaticPropsDetail component
-    // will receive `item` as a prop at build time
-    return { props: { item } }
-  } catch (err: any) {
-    return { props: { errors: err.message } }
-  }
+  	try {
+    	const id = params?.id
+    	const item = sampleUserData.find((data) => data.id === Number(id))
+    	// By returning { props: item }, the StaticPropsDetail component
+    	// will receive `item` as a prop at build time
+    	return { props: { item } }
+  	} catch (err: any) {
+    	return { props: { errors: err.message } }
+  	}
 }
