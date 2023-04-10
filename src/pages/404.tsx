dd2f5956b9e22a53f3f227ab161a5eb0bb869828
CrispyyBaconx <immabaconback@gmail.com>
2023-04-10 12:43:09 -0400

    Small Changes

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -12,11 +12,11 @@ const Error404 = () => {
             </Head>
             <div className="flex flex-col items-center justify-center h-screen bg-black">
                 <Link href="/" className="absolute top-12 left-12 m-4 text-4xl text-white transition ease-in-out hover:-translate-x-2 z-10 hover:drop-shadow-sm" title='Go Home'>&lt;</Link>
-                <div className="flex flex-col items-center p-10 px-16 rounded-3xl z-10 bg-dot">
+                <div className="flex flex-col items-center p-10 px-16 rounded-3xl z-10">
                     <h1 className="text-6xl font-bold text-white">Error <span className='text-red-500'>404</span></h1>
                     <h2 className="text-2xl font-bold text-white">Page Not Found</h2>
                 </div>
-                <p className='absolute text-[48rem] z-0 text-[#080808] leading-3 select-none'>404</p>
+                <p className='absolute text-[48rem] z-0 text-[#101010] leading-3 select-none'>404</p>
             </div>
         </>
     )
