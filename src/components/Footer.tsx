9d7f04f1690969adeb163df1e274508fd0169c84
CrispyyBaconx <immabaconback@gmail.com>
2023-04-13 01:33:19 -0400

    Updated Landing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 
 import logo from '@/assets/logo.svg';
 import DiscordLogo from '@/assets/DiscordLogo.svg';
+import TelegramLogo from '@/assets/TelegramLogo.svg';
 
 const Footer = () => {
     return (
@@ -32,7 +33,7 @@ const Footer = () => {
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Community</h2>
                             <ul className="text-gray-600 dark:text-gray-400 font-medium">
                                 <li className="mb-4">
-                                    <Link href="https://github.com/stakex" className="hover:underline ">Twitter</Link>
+                                    <Link href="https://github.com/stakex" className="hover:underline">Twitter</Link>
                                 </li>
                                 <li>
                                     <Link href="https://discord.gg/" className="hover:underline">Discord</Link>
@@ -65,7 +66,8 @@ const Footer = () => {
                             <span className="sr-only">Discord server</span>
                         </Link>
                         <Link href="https://telegram.com/" className="text-gray-500 hover:text-gray-900 dark:hover:text-white hover:scale-150 transition motion-reduce:transition-none motion-reduce:hover:transform-none">
-                            <span className="sr-only">GitHub account</span>
+                            <Image className='w-5 h-5' src={TelegramLogo} alt='Telegram Logo' />
+                            <span className="sr-only">Telegram account</span>
                         </Link>
                         <Link href="https://github.com/stakex" className="text-gray-500 hover:text-gray-900 dark:hover:text-white hover:scale-150 transition motion-reduce:transition-none motion-reduce:hover:transform-none">
                             <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z" clipRule="evenodd" /></svg>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,20 +22,20 @@ const Landing = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 			<main className='App'>
-				<section className='text-white' style={background}>
-					<div className='bg-gradient-to-r from-black px-8 py-16'>
-						<div className='max-w-xl grid grid-cols-1 gap-8'>
+				<section className='text-white object-contain w-full h-[50rem]' style={background}>
+					<div className='bg-gradient-to-r from-black px-28 py-16 h-full'>
+						<div className='max-w-xl grid grid-cols-1 gap-16'>
 							<div className='w-12'>
 								<Image src={Logo} alt='Stakex Logo' />
 							</div>
-							<h2 className='text-m uppercase font-bold'>Initializing...</h2>
+							<h2 className='text-m uppercase font-bold'>Initializing...</h2> {/* want to make this animated, or maybe just change color */}
 							<h1 className='text-6xl font-bold'>Stakex</h1>
 							<br />
-							<p>The best decentralized casino</p>
+							<p className='mb-12'>The best decentralized casino</p>
 							<div className='flex gap-6'>
-								<Link href='/app' className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2'>Enter App</Link>
-								<Link href='/stake' className='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
-									<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Stake →</span>
+								<Link href='/app' className='text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 shadow-lg shadow-purple-500/50 dark:shadow-lg dark:shadow-purple-800/80 font-medium rounded-lg text-sm px-10 py-5 text-center mr-2 mb-4'>Enter App</Link>
+								<Link href='/stake' className='relative inline-flex items-center justify-center p-0.5 mb-4 mr-4 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
+									<span className="relative px-10 py-5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">Stake →</span>
 								</Link>
 							</div>
 						</div>
@@ -53,16 +53,12 @@ const Landing = () => {
 						<h3 className='text-2xl font-bold mb-2'>
 							Filler
 						</h3>
-						<p className='text-lg'>
-							Written by <code>Filler</code>
-						</p>
 					</div>
 				</section>
 
 				<section className='px-8 py-16 bg-inset text-white'>
-					<div className='flex items-center'>
-						<h2>Want more Filler updates?</h2>
-						<br />
+					<div className='flex items-center gap-8'>
+						<h2>Want more updates?</h2>
 						<input type='text' className='bg-black' />
 					</div>
 				</section>
