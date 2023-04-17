45d6e2cf515879ec4808a4ddf1be79a56e6bac5b
CrispyyBaconx <immabaconback@gmail.com>
2023-04-17 10:26:30 -0400

    Updates

--- src/components/App/Breadcrumb.tsx
+++ src/components/App/Breadcrumb.tsx
@@ -11,9 +11,12 @@ const Breadcrumb = () => {
         let basePath = '/app';
         let currentPath = router.asPath;
 
+        console.log(currentPath);
+        console.log(currentPath.substring(currentPath.indexOf(basePath)));
         console.log(currentPath.substring(currentPath.indexOf(basePath)).split('/'));
 
         return currentPath.substring(currentPath.indexOf(basePath)).split('/');
+        // this returns [ "", "app" ]
     }
 
     return (
@@ -26,9 +29,12 @@ const Breadcrumb = () => {
                     </Link>
                 </li>
                 {getBreadcrumbPath(router).map((path, index) => {
+                    // maybe just do 
+                    // if (path === '/app') return; and skip the parsing
+
                     if(index === getBreadcrumbPath(router).length) {
                         return (
-                            <li aria-current="page">
+                            <li aria-current="page" key={index}>
                                 <div className="flex items-center">
                                     <svg aria-hidden="true" className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                     <span className="ml-1 text-sm font-medium text-gray-500 md:ml-2 dark:text-gray-400">{path}</span>
@@ -38,7 +44,7 @@ const Breadcrumb = () => {
                     }
 
                     return (
-                        <li>
+                        <li key={index}>
                             <div className="flex items-center">
                                 <svg aria-hidden="true" className="w-6 h-6 text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                                 <Link href="#" className="ml-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ml-2 dark:text-gray-400 dark:hover:text-white">{path}</Link>
