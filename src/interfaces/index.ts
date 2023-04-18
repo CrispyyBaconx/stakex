8df6d094fe54472aed898d697a059c51ab7db15f
CrispyyBaconx <immabaconback@gmail.com>
2023-04-18 09:42:56 -0400

    Added Address Type

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,12 @@
-export type User = {
-    id: number
+import { ethers } from "ethers"
+
+type Nominal<T extends string> = { readonly [k in T as `__${k}__`]: void }
+
+const isAddress = (val: string): val is Address => ethers.isAddress(val)
+
+export type Address = string & Nominal<"address">
+
+export type Member = {
+    wallet: Address
     name: string
 }
