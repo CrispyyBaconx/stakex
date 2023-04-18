49d95f262ecf934f4ace6092a4b547c9b8cc8cbb
CrispyyBaconx <immabaconback@gmail.com>
2023-04-18 13:49:44 -0400

    Fixed Sidebar

--- src/components/MinFooter.tsx
+++ src/components/MinFooter.tsx
@@ -6,7 +6,7 @@ const MinFooter = () => {
     return (
         <footer className="bg-white shadow dark:bg-gray-800">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="https://stakex.pro/" className="hover:underline">Stakex™</Link>. All Rights Reserved.</span>
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">Stakex™</Link>. All Rights Reserved.</span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
                         <Link href="/about" className="mr-4 hover:underline md:mr-6">About</Link>
@@ -15,7 +15,7 @@ const MinFooter = () => {
                         <Link href="/privacy" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
                     </li>
                     <li>
-                        <Link href="/" className="mr-4 hover:underline md:mr-6">Something</Link>
+                        <Link href="/terminal" className="mr-4 hover:underline md:mr-6">Something</Link>
                     </li>
                     <li>
                         <Link href="/contact" className="hover:underline">Contact</Link>
