de20c6374d7463b82006e1b581aa73c79b043e8e
CrispyyBaconx <immabaconback@gmail.com>
2023-04-20 15:17:30 -0400

    Update App

--- src/pages/app/index.tsx
+++ src/pages/app/index.tsx
@@ -1,6 +1,7 @@
 import MinFooter from "@/components/MinFooter";
 import Head from "next/head";
 import Image from "next/image";
+import Link from 'next/link';
 
 import FootballCard from '@/assets/FootballCard.jpg';
 
@@ -8,6 +9,12 @@ import { Sidebar } from '@/components/App';
 import Card from "@/components/App/Card";
 
 const App = () => {
+    // https://www.quackit.com/css/flexbox/examples/flexbox_website_layout_examples.cfm
+    // https://www.quackit.com/html/html_editors/scratchpad/?example=/css/flexbox/examples/flexbox_website_layout_2
+    // https://www.quackit.com/html/html_editors/scratchpad/?example=/css/flexbox/examples/flexbox_website_layout_4
+    // https://tailwindcss.com/blog/tailwindcss-v3-3
+    // https://tailwindcss.com/docs/grid-template-columns
+
     return (
         <>
             <Head>
@@ -18,8 +25,14 @@ const App = () => {
             </Head>
             <main className="flex bg-gray-900">
                 <Sidebar />
-                <header className="flex p-8 w-full">
-                    <h2 className="text-white text-4xl">Stakex</h2>
+                <header className="flex p-8 w-full justify-center">
+                    <div>
+                        <h2 className="text-white text-4xl">Stakex</h2>
+                        <h5 className="p-1">Beta</h5>
+                    </div>
+                    <div className="p-4">
+                        <Link href='/stake'>Water</Link>
+                    </div>
                 </header>
                 <div className="flex flex-row gap-4 p-4 bg-inset h-40 w-full items-center">
                     <Card sport="Football" description="David" image={ FootballCard } />
