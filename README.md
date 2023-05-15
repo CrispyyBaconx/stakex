f70f32de1185e337b9dc2a7c86173565880e6d70
CrispyyBaconx <immabaconback@gmail.com>
2023-05-15 01:31:51 -0400

    Migrated to a T3 App

---
+++ README.md
@@ -0,0 +1,30 @@
+# Stakex
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/)
+
+## How to use it?
+
+To run a dev server
+```bash
+npm run dev
+```
+
+To build a static page
+```bash
+npm run build
+```
+
+Vercel [documentation](https://nextjs.org/docs/deployment)
+
+## Tech Stack
+
+- [T3](https://create.t3.gg/)
+
+## Authors
+
+Bacon#2077 - [Twitter](https://twitter.com/CrispyyBaconx)
+
+### Todo: 
+
+[ ]: Terminal with puzzle
+[ ]: Prediction markets
---
+++ tailwind.config.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import plugin from 'tailwindcss/plugin';
+import { fontFamily } from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				inset: '#0d0e11',
+				accent: '#02070c',
+			},
+			fontFamily: {
+				sans: ['var(--font-blender-pro)', ...fontFamily.sans],
+			},
+			textShadow: {
+				sm: '0 1px 2px var(--tw-shadow-color)',
+				DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+				lg: '0 8px 16px var(--tw-shadow-color)',		
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'dot': "radial-gradient(rgba(255, 255, 255, 0.2) 1px, transparent 1px)",
+			},
+			animation: {
+				border: 'background ease infinite',
+			},
+			keyframes: {
+				background: {
+				  	'0%, 100%': { backgroundPosition: '0% 50%' },
+				  	'50%': { backgroundPosition: '100% 50%' },
+				},
+			},		  
+		},
+	},
+	plugins: [
+		plugin(function ({ matchUtilities, theme }) {
+			matchUtilities (
+			  	{
+					'text-shadow': (value) => ({
+				  		textShadow: value,
+					}),
+			  	},
+			  	{ values: theme('textShadow') }
+			)
+		}),	  
+	],
+}
