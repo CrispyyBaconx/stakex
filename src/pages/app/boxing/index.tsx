bb5df898e7a8dd5cfad3a9f9974cf083cc3a4b2f
CrispyyBaconx <immabaconback@gmail.com>
2023-05-16 14:43:29 -0400

    Added Q

--- src/pages/app/boxing/index.tsx
+++ src/pages/app/boxing/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const index = () => {
+const Boxing = () => {
     return (
         <div>index</div>
     )
 }
 
-export default index;
+export default Boxing;
+// instead of doing this as individual pages for each sport, why not just make a single page and pass in the sport as a prop?
+// and then fill in the page with the appropriate data based on the prop passed in?
