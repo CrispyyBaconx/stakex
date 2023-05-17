d1b8afe2a3c7e834cfedc2f5a58630c2b4f3faa1
CrispyyBaconx <immabaconback@gmail.com>
2023-05-17 10:25:01 -0400

    Added Betslip

---
+++ src/components/App/Betslip.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Betslip = () => {
+    return (
+        <div className="flex flex-col flex-grow w-full max-w-7xl mx-auto md:px-8 xl:px-0">
+            <div className="flex text-3xl">
+                {/* Maybe put an svg */}
+                Betslip
+            </div>
+            <div>
+                {/* Betslip - user can add */}
+            </div>
+        </div>
+    )
+}
+    
+export default Betslip;
