9372fcc35bdec15c8fd24b555c52fcbcf64d70fa
CrispyyBaconx <immabaconback@gmail.com>
2023-05-17 09:30:16 -0400

    Added Inspiration

--- src/components/MinFooter.tsx
+++ src/components/MinFooter.tsx
@@ -6,7 +6,7 @@ const MinFooter = () => {
     return (
         <footer className="bg-white shadow dark:bg-gray-800 z-9999 flex">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">Stakex™</Link>. All Rights Reserved.</span>
+                <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">Stakex™</Link> All Rights Reserved.</span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
                         <Link href="/about" className="mr-4 hover:underline md:mr-6">About</Link>
