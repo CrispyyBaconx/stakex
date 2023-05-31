5da9e7d0dcf6e813cc873e763f8fac5dc5ede2fc
CrispyyBaconx <immabaconback@gmail.com>
2023-05-31 04:30:44 -0400

    Worked on Homepage, Didn't finish
    Still need to make a gif
    Fix FAQ
    Fix Info Section Spacing and stuff
    Add Logo
    Fix Hero

--- src/components/Landing/FAQ.tsx
+++ src/components/Landing/FAQ.tsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const FAQ = () => {
     return (
-        <>
-            Implement me!
-        </>
+        <div>
+            Instant Betting: Place bets on your favorite teams and players without any unnecessary steps - no deposits, no registrations.
+
+            Transparency: With Stakex, your bets are recorded on-chain, ensuring complete transparency and fairness. You can trust that there's no manipulation or unfair practices.
+
+            Low Fees: Thanks to the scalability solutions provided by Arbitrum One, you can enjoy incredibly low fees, making your betting experience even more rewarding.
+
+            Real-Time Updates: Stakex integrates real-time odds and updates, ensuring you have the most current information at your fingertips.
+
+            Secure: Your funds are yours alone. We leverage the power of smart contracts to ensure your winnings are transferred directly to your wallet - no middleman, no waiting.
+        </div>
     )
 }
 
--- src/components/Landing/index.ts
+++ src/components/Landing/index.ts
@@ -1,3 +1,2 @@
 export { default as FAQ } from './FAQ';
-export { default as Initializing } from './Initializing';
 export { default as Newsletter } from './Newsletter';
