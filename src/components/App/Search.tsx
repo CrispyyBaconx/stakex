cdc40e0a24cf3545cdd694720d2c864f34b5d6fa
CrispyyBaconx <immabaconback@gmail.com>
2023-06-03 02:10:51 -0400

    Upgraded Search, Implemented API Route
    This build will fail and I know, just updating changes

--- src/components/App/Search.tsx
+++ src/components/App/Search.tsx
@@ -1,14 +1,43 @@
+import { useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
+import { api } from '@/utils/api';
+import { type Game } from '@prisma/client';
 
 const Search = () => {
+    const [searchString, setSearchString] = useState("");
+    const { isLoading, data: searchResults } = api.main.searchGames.useQuery(
+      { searchString },
+      { enabled: !!searchString }
+    );
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchString(event.target.value);
+    }
+
     return (
         <div className='p-4 flex flex-row items-center'>
             <div className='relative w-96 h-10 bg-slate-600 rounded-md'> {/* Container acting as the input field */}
                 <BiSearch className='absolute top-1/2 left-3 transform -translate-y-1/2 text-gray-500' />
-                <input className='w-full h-full p-2 pl-8 text-gray-900 bg-transparent rounded-md' type="text" placeholder='Search' />
+                <input 
+                  className='w-full h-full p-2 pl-8 text-gray-900 bg-transparent rounded-md' 
+                  type="text" 
+                  placeholder='Search' 
+                  value={searchString} 
+                  onChange={handleSearch} 
+                />
             </div>
+            {!!searchResults && (
+              <div>
+                {searchResults.map(((game: Game) => (
+                  <article key={game.id}>
+                        <h1>Game Found</h1>
+                  </article>
+                )))}
+              </div>
+            )}
         </div>
-    ) 
+    )
 }
+// ! Add a loading state for when the search is loading
 
 export default Search;
--- src/server/api/routers/app.ts
+++ src/server/api/routers/app.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { type Game } from "@prisma/client";
 
 export const mainRouter = createTRPCRouter({
 	getGames: publicProcedure.query(async ({ ctx }) => {
-		// ! maybe instead return a list of games by sport and next games by time & sport
 		const games = await ctx.prisma.game.findMany({
 			take: 100,
 		});
@@ -24,6 +24,29 @@ export const mainRouter = createTRPCRouter({
 		});
 
 		return games.map((game) => {
+			return {
+				...game,
+			}
+		});
+	}),
+	searchGames: publicProcedure
+		.input(z.object({
+				searchString: z.string().min(2), // at least 2 characters before searching
+	  		})
+		)
+		.query(async ({ ctx, input }) => { // I could return the games I found with the query, and then use the string current and compare it to make a highlight effect in the frontend
+		const games = await ctx.prisma.game.findMany({
+			where: {
+				OR: [
+					{ name: { contains: input.searchString }}, // need to fix schema in db, and also fix this since time won't work without some sort of mutation
+					{ team: { contains: input.searchString }}, // also can be team a or team b
+					{ time: { contains: input.searchString }}, // after that need to implement args in the frontend to pass in the search string
+				],
+			},
+			take: 100,
+		});
+
+		return games.map((game: Game) => {
 			return {
 				...game,
 			}
