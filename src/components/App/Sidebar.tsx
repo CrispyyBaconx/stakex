8210f0462318e35b5194937c9d9be28e3064d2e3
CrispyyBaconx <immabaconback@gmail.com>
2023-06-09 03:58:33 -0400

    Change To Sidebar Component

--- src/components/App/Sidebar.tsx
+++ src/components/App/Sidebar.tsx
@@ -4,20 +4,9 @@ const Sidebar = () => {
     return (
         <>
             <aside className="top-0 left-0 z-10 w-16 h-screen transition-transform -translate-x-full sm:translate-x-0">
-               	<div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800 no-scrollbar">
-                  	<Link href="/" className="flex items-center pl-1 mb-5" />
-                  	<ul className="space-y-2 font-medium">
-                     	<li>
-                        	<Link href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                           		<svg className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
-                        	</Link>
-                     	</li>
-                     	<li>
-                     	   	<Link href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
-                     	   	   	<svg className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-                     	   	</Link>
-                     	</li>
-                  	</ul>
+               	<div className="h-full w-36 px-6 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-950 no-scrollbar">
+					{/* Implement backend? or just a static list of games / inplay*/}
+					<Link href="/in-play">In Play</Link>
                	</div>
             </aside>
         </>
