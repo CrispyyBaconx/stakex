f635601cbb7360d27013767b23fa6d56ffdc121b
CrispyyBaconx <immabaconback@gmail.com>
2023-06-20 04:58:37 -0400

    Updates to the Staking Page

--- src/pages/stake/index.tsx
+++ src/pages/stake/index.tsx
@@ -100,38 +100,42 @@ const Stake = () => {
                     {stake ? (
                         <div className='flex flex-col items-center'>
                             <div className='flex flex-col items-center mt-12 w-[70em] justify-around'>
-                                <div className='flex flex-row p-10 mx-8 leading-7 bg-gray-800 border-2 rounded-[20px] border-gray-800 gap-2'>
+                                <div className='flex flex-row p-10 mx-8 leading-7 bg-gray-800 border-2 rounded-[20px] border-gray-800 gap-2 relative'>
+                                    {account && etherBalance ? null : (
+                                        <div className='z-50'>
+                                            <div className="absolute h-full w-full rounded-xl inset-0 backdrop-blur-lg" />
+                                            <div className="text-overlay absolute inset-0 flex flex-col justify-center items-center text-white text-center">
+                                                <h1 className="text-2xl font-bold text-slate-400"><span className='text-slate-500'>&lt;</span> Please Connect Your Wallet <span className='text-slate-500'>&gt;</span></h1>
+                                            </div>
+                                        </div>
+                                    )}
                                     <div className='flex flex-col gap-4 mr-8 w-48'> {/* staking */}
                                         <p className='text-gray-400 text-2xl self-center'>Stake</p>
-                                        
                                         <label className='relative inline-flex border border-solid border-slate-500 rounded-2xl items-stretch box-border px-3 cursor-text transition-colors duration-100 ease-in-out w-full self-center'>
                                             <span className='flex items-center flex-shrink-0 cursor-inherit pr-4'>
                                                 {/* eslint-disable-next-line @typescript-eslint/no-unsafe-assignment */}
                                                 <Image src={Logo} alt='Stakex Logo' width={24} height={24} />
                                             </span>
-
                                             <span className='font-normal text-base flex flex-grow relative py-4'>
                                                 <input className='bg-transparent shadow-none border-transparent outline-none w-full font-normal text-base leading-normal p-0 rounded-none relative top-2' disabled={false} placeholder='0.00' min={0} value={stakeAmount} onChange={handleStakeAmountChange} />
                                                 <span className='absolute left-0 top-1/2 text-base leading-tight whitespace-nowrap overflow-hidden text-ellipsis max-w-full transform origin-top translate-y-[-4px] translate-x-[-6px] scale-75 opacity-100 -mt-4 duration-100 ease-in-out'>Amount</span>
                                             </span>
-
                                             <span className='flex items-center flex-shrink-0 cursor-inherit pl-4'>
                                                 <div className='flex items-center'>
-                                                    <button className='border-2 border-purple-800 outline-none bg-transparent px-1 bg-purple-800 text-purple-300 opacity-70 rounded-lg' onClick={() => { setStakeAmount(balanceHeld ?? 0) }}>
+                                                    <button className='border-2 border-blue-800 outline-none bg-transparent px-1 text-blue-300 opacity-70 rounded-lg' onClick={() => { setStakeAmount(balanceHeld ?? 0) }}>
                                                         <p className='font-bold text-xs p-1'>MAX</p> {/* make this actually functional */}
                                                     </button>
                                                 </div>
                                             </span>
                                         </label>
-
-                                        <button className='flex text-white bg-blue-500 p-4 rounded-xl w-full h-16 justify-center' onClick={() => { sendTransaction({ to: poolAddress, data: '0x' }).then().catch(console.error) }}>
-                                            <p className='text-l self-center'>Stake</p>
+                                        <button className="relative inline-flex items-center justify-center p-0.5 w-full h-16 overflow-hidden text-sm font-medium text-gray-900 rounded-xl group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800" onClick={() => { sendTransaction({ to: poolAddress, data: '0x' }).then().catch(console.error) }}>
+                                            <span className="relative flex justify-center items-center w-full h-full transition-all ease-in duration-75 bg-gray-900 rounded-xl group-hover:bg-opacity-0 text-lg">
+                                                Stake
+                                            </span>
                                         </button>
                                     </div>
-
                                     {/* separator */}
                                     <div className='border-r-2 border-gray-600 h-auto' />
-
                                     <div className='flex flex-col gap-4 mx-8 w-48'> {/* unstaking */}
                                         <p className='text-gray-400 text-2xl self-center'>Unstake</p>
                                         <label className='relative inline-flex border border-solid border-slate-500 rounded-2xl items-stretch box-border px-3 cursor-text transition-colors duration-100 ease-in-out w-full self-center'>
@@ -139,45 +143,34 @@ const Stake = () => {
                                                 <input className='bg-transparent shadow-none border-transparent outline-none w-full font-normal text-base leading-normal p-0 rounded-none relative top-2' disabled={false} placeholder='0.00' min={0} value={unstakeAmount} onChange={handleUnstakeAmountChange} />
                                                 <span className='absolute left-0 top-1/2 text-base leading-tight whitespace-nowrap overflow-hidden text-ellipsis max-w-full transform origin-top translate-y-[-4px] translate-x-[-6px] scale-75 opacity-100 -mt-4 duration-100 ease-in-out'>Amount</span>
                                             </span>
-
                                             <span className='flex items-center flex-shrink-0 cursor-inherit pl-4'>
                                                 <div className='flex items-center'>
-                                                    <button className='border-2 border-purple-800 outline-none bg-transparent px-1 bg-purple-800 text-purple-300 opacity-70 rounded-lg' onClick={() => { setUnstakeAmount(balanceStaked ?? 0) }}>
+                                                    <button className='border-2 border-blue-800 outline-none bg-transparent px-1 text-blue-300 opacity-70 rounded-lg' onClick={() => { setUnstakeAmount(balanceStaked ?? 0) }}>
                                                         <p className='font-bold text-xs p-1'>MAX</p> {/* make this actually functional */}
                                                     </button>
                                                 </div>
                                             </span>
                                         </label>
-
-                                        <button className='flex text-white bg-blue-500 p-4 rounded-xl w-full h-16 justify-center' onClick={() => { sendTransaction({ to: poolAddress, data: '0x' }).then().catch(console.error) }}>
-                                            <p className='text-l self-center'>Unstake</p>
+                                        <button className="relative inline-flex items-center justify-center p-0.5 w-full h-16 overflow-hidden text-sm font-medium text-gray-900 rounded-xl group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800" onClick={() => { sendTransaction({ to: poolAddress, data: '0x' }).then().catch(console.error) }}>
+                                            <span className="relative flex justify-center items-center w-full h-full transition-all ease-in duration-75 bg-gray-900 rounded-xl group-hover:bg-opacity-0 text-lg">
+                                                Unstake
+                                            </span>
                                         </button>
                                     </div>
-
                                     {/* separator */}
                                     <div className='border-r-2 border-gray-600 h-auto' />
-
                                     <div className='flex flex-col gap-4 ml-8 w-48 justify-between'> {/* claim */}
                                         <p className='text-gray-400 text-2xl self-center'>Claim</p>
                                         <p className='text-white text-sm self-center'>Total Claimable: {balanceClaimable ?? 0.0}</p>
-                                        {account && etherBalance ? ( // make this apply to the whole panel instead of just the claim button (maybe blur the background too?)
-                                            <div className='flex'>
-                                                <button className='flex text-white bg-blue-500 p-4 rounded-xl w-full h-16 justify-center mt-1' onClick={() => { sendTransaction({ to: poolAddress, data: '0x' }).then().catch(console.error) }}>
-                                                    <p className='text-l self-center'>Claim</p>
-                                                </button>
-                                            </div>
-                                        ) : (
-                                            <div className='flex opacity-70 bg-slate-500 border-[1px] border-slate-600 rounded-xl w-full justify-center h-16 mt-1'>
-                                                {/* diagonal line */}
-                                                <div className='border-r-2 border-slate-400 h-12 transform rotate-45 relative self-center' />
-                                                <div className='border-r-2 border-slate-400 h-12 transform rotate-45 relative left-12 self-center' />
-                                                <p className='text-white self-center z-50 font-bold text-shadow shadow-slate-200'>Please Connect</p>
-                                                <div className='border-r-2 border-slate-400 h-12 transform rotate-45 relative self-center' />
-                                            </div>
-                                        )}
+                                        <div className='flex'>
+                                            <button className="relative inline-flex items-center justify-center p-0.5 w-full h-16 overflow-hidden text-sm font-medium text-gray-900 rounded-xl group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800" onClick={() => { sendTransaction({ to: poolAddress, data: '0x' }).then().catch(console.error) }}>
+                                                <span className="relative flex justify-center items-center w-full h-full transition-all ease-in duration-75 bg-gray-900 rounded-xl group-hover:bg-opacity-0 text-lg">
+                                                    Claim
+                                                </span>
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
-
                                 <div className='flex flex-row gap-4 p-4 mt-12 rounded-xl w-full bg-slate-700 justify-between'>
                                     <p className='text-slate-400'>FAQ</p>
                                     {/* going to be a drop down into an accordian is what im thinkin */}
@@ -200,14 +193,14 @@ const Stake = () => {
                                     <div className='flex flex-col items-center'>
                                         {apy} <span>% APY</span>
                                     </div>
-                                ) : 
+                                ) : (
                                     <div role="status" className="max-w-sm animate-pulse">
                                         <div className="h-3 bg-gray-200 rounded-full dark:bg-gray-700 w-8 mb-4" />
                                         <span className="sr-only">
                                             <LoadingSpinner size={16} /> {/* Change name to skeleton */}
                                         </span>
                                     </div>
-                                }
+                                )}
                             </div>
                         </div>
                     )}
