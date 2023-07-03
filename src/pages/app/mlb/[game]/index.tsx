b4b37fc24235d4c3a029721f220cb9a30e51bf89
CrispyyBaconx <immabaconback@gmail.com>
2023-07-03 03:17:10 -0400

    Added Some Notes
    Im Taking A Small Break

--- src/pages/app/mlb/[game]/index.tsx
+++ src/pages/app/mlb/[game]/index.tsx
@@ -15,3 +15,7 @@ const Game = () => {
 export default Game;
 
 // this is a catchall for the mlb games, it will render the appropriate game page based on the url
+
+// game -> request game data from server -> have all markets/odds for that game currently available -> organize data by category (moneyline, total, etc) -> render
+// also if I am going to catchall the games and query what game it is by url I need to make sure that isn't a security issue
+// ref: https://stackoverflow.com/questions/15917400/how-dangerous-is-a-mongo-query-which-is-fed-directly-from-a-url-query-string
