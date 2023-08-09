16eed4324125a55918a9c0312f8e85dce0ede760
CrispyyBaconx <immabaconback@gmail.com>
2023-08-09 05:43:55 -0400

    Update Carousel & Game Page

--- src/pages/app/tennis/[game]/index.tsx
+++ src/pages/app/tennis/[game]/index.tsx
@@ -1,7 +1,20 @@
-const index = () => {
+import { GamePage } from "@/components/App";
+import { useRouter } from "next/router";
+
+const Game = () => {
+    const router = useRouter();
+    const { game } = router.query; // this is the game string from the url /app/tennis/[game]
+    const category = router.pathname.split('/')[2]; // this is the category string from the url /app/(category)/[game]
+
+    // compute assets based on category and pass them to the game component
+
+    // if query(game) is not a valid game, redirect to the 404 page
+
     return (
-        <div>index</div>
+        <div>
+            test
+        </div>
     )
 }
 
-export default index;
+export default Game;
