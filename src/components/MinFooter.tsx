4fa57e91efa47069fd71578f2986f69d4f9d7c06
CrispyyBaconx <immabaconback@gmail.com>
2023-10-20 20:50:15 -0400

    Fix Footer

--- src/components/MinFooter.tsx
+++ src/components/MinFooter.tsx
@@ -13,9 +13,6 @@ const MinFooter = () => {
                         <Link href="/privacy-and-tos" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
                     </li>
                     <li>
-                        <Link href="/terminal" className="mr-4 hover:underline md:mr-6">Terminal</Link>
-                    </li>
-                    <li>
                         <Link href="/contact" className="hover:underline">Contact</Link>
                     </li>
                 </ul>
